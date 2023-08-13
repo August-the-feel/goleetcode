ce2375c7fd918a3a17761c393525124912394e77
张雨泽 <for_all_I_crea@163.com>
2023-08-13 23:57:44 +0800

    上传代码--84注解

---
+++ 67/go.mod
@@ -0,0 +1,3 @@
+module goleetcode/67
+
+go 1.20
--- 84/main.go
+++ 84/main.go
@@ -15,26 +15,32 @@ func largestRectangleArea(heights []int) int {
 	for i := 0; i < n; i++ {
 		right[i] = n
 	}
-	// 创建一个堆
+	// 创建一个单调栈 用于记录递增的柱子的索引。
 	mono_stack := []int{}
 	for i := 0; i < n; i++ {
-		// mono_stack
+		// 如果当前柱子的高度小于等于栈顶元素所对应的柱子的高度，那么栈顶元素就不可能作为矩形的右边界了。因此，将栈顶元素所对应的柱子的右边界
+		// 		设为当前柱子的索引， 并将栈顶元素出栈，直到栈顶元素的高度小于当前柱子的高度。
+		// mono_stack[len(mono_stack)-1] 栈顶元素下标
 		for len(mono_stack) > 0 && heights[mono_stack[len(mono_stack)-1]] >= heights[i] {
-			fmt.Println(heights[mono_stack[len(mono_stack)-1]])
-			// 右边界
+			// 满足 heights[mono_stack[len(mono_stack)-1]] >= heights[i] 栈顶的右边界 一直在 变化
 			right[mono_stack[len(mono_stack)-1]] = i
 			mono_stack = mono_stack[:len(mono_stack)-1]
 		}
+		// 左边的边界
 		if len(mono_stack) == 0 {
+			// 如果栈为空，则将当前柱左边界为 -1。
 			left[i] = -1
 		} else {
+			// 如果栈不为空，那么栈顶元素所对应的柱子就是当前柱子的左边界。
 			left[i] = mono_stack[len(mono_stack)-1]
 		}
 		mono_stack = append(mono_stack, i)
-		fmt.Println(mono_stack)
 	}
+
+	// 创建一个最大值
 	ans := 0
 	for i := 0; i < n; i++ {
+		// (right[i]-left[i]-1)*heights[i] 求最大值 right[i]-left[i]-1 左边界 从-1 开始
 		ans = max(ans, (right[i]-left[i]-1)*heights[i])
 	}
 	return ans
